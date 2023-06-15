0d759e6156a507cb9af7460d6d8aa7ad76cb18d2
Kapsulon <theo.bary@epitech.eu>
2023-06-15 17:21:59 +0200

    ADD: funny

--- src/lib/studentcard.svelte
+++ src/lib/studentcard.svelte
@@ -1,12 +1,25 @@
 <script lang="ts" context="module">
     import { getJsonstudent } from "$lib/jsonparser.svelte";
-    export function createCards():any {
-        getJsonstudent().then((data) => {
-            let students = document.createElement("div");
-            students.classList.add("students");
-            for (let key in data) {
-                console.log(data[key]);
+    export async function createCards():Promise<void> {
+        let data: any = await getJsonstudent()
+        let students = document.createElement("div");
+        students.classList.add("students");
+        for (let key in data) {
+            let student = students.appendChild(document.createElement("div"));
+            student.classList.add("student");
+            let emoji = student.appendChild(document.createElement("span"));
+            if (!data[key].out) {
+                emoji.classList.add("spin");
+                emoji.innerHTML = "🤙";
+            } else {
+                emoji.classList.add("out");
+                emoji.innerHTML = "👻";
             }
-        });
+            let info = student.appendChild(document.createElement("ul"));
+            info.id = "info";
+            let name = info.appendChild(document.createElement("li"));
+            name.innerHTML = data[key].login.split("@")[0].replace(".", " ").toUpperCase();
+        }
+        document.body.appendChild(students);
     }
 </script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import { createCards } from "$lib/studentcard.svelte";
+    import twemoji from "twemoji";
     onMount(() => {
-        createCards();
+        createCards().then(() => {
+            twemoji.parse(document.body);
+        });
     });
 </script>
 
@@ -10,21 +13,5 @@
     <head>
         <title>Pointos</title>
         <link rel="stylesheet" href="src/style/main.scss">
-        <script src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js" crossorigin="anonymous"></script>
     </head>
-    <body>
-        <div class="students">
-            <div class="student">
-                <span class="spin">🤙</span>
-                <ul id="info">
-                    <li>test2</li>
-                    <li>test3</li>
-                    <li>test4</li>
-                </ul>
-            </div>
-        </div>
-    </body>
-    <script>
-        twemoji.parse(document.body);
-    </script>
 </html>
