2a9ab9c0c8cef236be825460955a0b75bf87e05d
zagreus <alberic.flour-de-saegher@epitech.eu>
2023-06-15 15:41:17 +0200

    ADD: parse json

---
+++ src/lib/jsonparser.svelte
@@ -0,0 +1,9 @@
+<script lang="ts" context="module">
+import { readFileSync } from 'fs';
+/* fill array json */
+
+export function getJsonstudent(fileName: string): object | null {
+    const file = readFileSync(fileName, 'utf-8');
+    return JSON.parse(file);
+}
+</script>
